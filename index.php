0fa962fcd48bb560bcfd61cba84aa749a8e3962c
ynikulyak <45584946+ynikulyak@users.noreply.github.com>
2019-07-07 22:06:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>Hangman</title>
         <link  href="css/styles.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
